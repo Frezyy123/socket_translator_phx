7935393f9aad498e886ae63567795b20301825ef
Aleksandr Vinogradov <Frezyy123@gmail.com>
2020-08-12 21:18:18 +0300

    Add httpoison to dependencies Create api module Translator

---
+++ lib/socket_translator_phx/translator.ex
@@ -0,0 +1,35 @@
+defmodule SocketTranslator.Translator do
+  @api_url "https://translate.api.cloud.yandex.net/translate/v2/translate"
+
+  @spec translate_message(String.t(), String.t()) :: {:ok, String.t()} | {:error, atom()}
+  def translate_message(message, _token \\ "") do
+    token = "CggVAgAAABoBMxKABLW9R3iMYkJjjI-1CogbUuPPgmDV7AERy_lcsnekg7bGkD9fUBzYLf_uFZMFNkHylFDk-NTkA0IZLb9aVqwLJ06y3Zse8lfkfVcsRrU43yeZxbxWV-jAU-l7MvCjvJl94D1QN_hhpU4yS1UyVUHQI753ZrKAW3zBnRpPaAEk9wfbzhOF4OX_VlSnt5bQ7uW9d7CBePMnpjvJK2CfyYz4sliW-jaE0iaMihQmBivgptPN847fgiTKOr6-4zm8BQQP0oqrD8Gdg77pHPaeFMMnmAI-sNd8g3bVwO-hHGxkW8gG813Pm_m7rB678eUt4pKOXdRdEs7aEJto8fOZoLk0vQRr1AkkDvssdPdaB4on4v6EY5vMfdfxBGgh6WE9KYdkzbrpY2AjB8gnYl86_knU8gri_vt-tInNS89BUGoCTFIrziEkWTWL5X_UnmimsDVa3CyfgepmTNr806aa0t9Tuv6tEfsUdfX4aGze38LFaqwEbwYLdqX_Jej3LQuhcBXTVQZTJgIS4aZ98wWbgK2qzWN50tMse9yOJ-NlqlBXVOKqYEM6glrd0LhGWiNEAb4VMHZjRCRWNhgtP9prh2MIiguwE01FFxoEDlOxnzL74xbBmiG_cZpKuCxptKzlO7mBE99Y02R4tCj9uJp0JdMpAetgGu8IwCvjaLU98TePG0zJGiQQktHQ-QUY0qLT-QUiFgoUYWplaWJsbzJwM29tdmN1azB2dGs="
+
+    headers = [{"Content-Type", "application/json"}, {"Authorization", "Bearer #{token}"}]
+
+    body =
+      %{
+        folder_id: "b1g75qo3ke15tavldsvv",
+        texts: message,
+        targetLanguageCode: "ru"
+      }
+      |> Jason.encode!()
+
+    case HTTPoison.post(@api_url, body, headers) do
+      {:ok, %HTTPoison.Response{status_code: 200} = response} -> parse_response(response)
+      {:error, %HTTPoison.Error{} = error} -> parse_error(error)
+    end
+  end
+
+
+  defp parse_response(%HTTPoison.Response{body: body}) do
+    result = Jason.decode!(body)
+
+    %{"translations" => translations} = result
+
+    Enum.reduce(translations, "", fn %{"text" => text}, acc -> acc  <> text <> " " end)
+    |> String.trim()
+  end
+
+  defp parse_error(%HTTPoison.Error{reason: reason}), do: {:error, reason}
+end
