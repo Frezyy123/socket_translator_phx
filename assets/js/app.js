52cc436506653daf59eb023e30876ea4f8bcdf42
Aleksandr Vinogradov <Frezyy123@gmail.com>
2020-08-13 23:43:44 +0300

    Some awful js for debugging

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,27 @@ import "../css/app.scss"
 // Import deps with the dep name or local files with a relative path, for example:
 //
 //     import {Socket} from "phoenix"
-//     import socket from "./socket"
+import socket from "./socket"
 //
 import "phoenix_html"
+
+let channel = socket.channel("translator", {})
+channel.join()
+  .receive("ok", resp => { console.log("Joined successfully", resp) })
+  .receive("error", resp => { console.log("Unable to join", resp) })
+
+channel.on('translate', function(payload) {
+    console.log(payload.eng_message)
+    let message_input = document.getElementById('message-input')
+    message_input.value = payload.eng_message
+});
+
+
+document.getElementById("submit-button").addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log(e)
+    let message_input = document.getElementById('message-input')
+    channel.push('translate', { message: message_input.value })
+    message_input.value = ""
+});
+
